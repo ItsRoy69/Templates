de791b5c1ae36d70f1a1209ecf1bc4351c8fea6d
ItsRoy69 <jyotirmoyroy649@gmail.com>
2022-09-23 08:30:27 +0530

    birds

---
+++ birds/script.js
@@ -0,0 +1,11 @@
+VANTA.BIRDS({
+    el: "body",
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: 200.00,
+    minWidth: 200.00,
+    scale: 1.00,
+    scaleMobile: 1.00,
+    color1: 0x7f6363
+})
